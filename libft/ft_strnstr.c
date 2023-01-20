9123f664b43a4c1336ac30a767b0c7a0b95e618f
Andrew <112986012+AerdnaAnimug@users.noreply.github.com>
2023-01-20 17:03:35 +0100

    Add files via upload

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agumina <agumina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/20 14:07:36 by agumina           #+#    #+#             */
-/*   Updated: 2023/01/20 16:28:44 by agumina          ###   ########.fr       */
+/*   Created: 2023/01/20 16:31:57 by agumina           #+#    #+#             */
+/*   Updated: 2023/01/20 17:00:36 by agumina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	n;
-	size_t	dlen;
-	size_t	slen;
 
 	i = 0;
-	n = 0;
-	while (dst[n] && n < dstsize)
-	{
-		n++;
-	}
-	dlen = n;
-	slen = ft_strlen(src);
-	while (src[i] && i < dstsize - dlen - 1)
-	{
-		dst[n] = src[i];
-		i++;
-		n++;
-	}
-	if (i < dstsize)
-		dst[n] = '\0';
-	return (dlen + slen);
+	while (haystack[i] && i < len)
+	
 }
