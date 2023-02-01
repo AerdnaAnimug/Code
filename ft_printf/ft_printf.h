be63ecffe0d66a23e7a9718f8f2b823aa37d9473
Andrew <112986012+AerdnaAnimug@users.noreply.github.com>
2023-02-01 17:40:52 +0100

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agumina <agumina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 15:22:42 by agumina           #+#    #+#             */
+/*   Updated: 2023/02/01 16:48:24 by agumina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINFT_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int ft_printf(const char *, ...);
+
+#endif
