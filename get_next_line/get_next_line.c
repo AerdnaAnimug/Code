6a75d76b3680c5ec5af38727925e3ee28a766657
Andrew <112986012+AerdnaAnimug@users.noreply.github.com>
2023-02-21 18:30:58 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agumina <agumina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 12:40:22 by agumina           #+#    #+#             */
-/*   Updated: 2023/02/17 17:02:14 by agumina          ###   ########.fr       */
+/*   Updated: 2023/02/21 16:06:16 by agumina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,39 @@
 
 char	*ft_read(int fd, char *save)
 {
-	int	r;
-	char	*buff;
+	int		r;
+	char	*buffer;
 
 	r = 1;
-	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
 		return (NULL);
-	while
+	while (!ft_strchr(save, '\n') && r != 0)
+	{
+		r = read(fd, buffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[r] = '\0';
+		save = ft_strjoin(save, buffer);
+	}
+	free(buffer);
+	return (save);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*save;
-	
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	save = ft_read(fd, save);
 	if (!save)
 		return (NULL);
-	line = 0; //ft_row(save)
-	save = 0; //ft_remove(save);
+	line = ft_row(save);
+	save = ft_remove(save);
 	return (line);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agumina <agumina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 13:06:02 by agumina           #+#    #+#             */
+/*   Updated: 2023/02/21 13:14:44 by agumina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -5,9 +17,9 @@
 # include <stdio.h>
 # include <unistd.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1000
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_read(int fd, char *save);
