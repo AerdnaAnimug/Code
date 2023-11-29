2922549848d7ebc85d441a40a39dd098bab255c5
Andrew <112986012+AerdnaAnimug@users.noreply.github.com>
2023-11-29 13:07:06 +0100

    Add files via upload

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: agumina <agumina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 12:34:46 by agumina           #+#    #+#             */
-/*   Updated: 2023/11/14 12:42:41 by agumina          ###   ########.fr       */
+/*   Updated: 2023/11/29 13:03:19 by agumina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,35 @@
 #include "../ft_printf/ft_printf.h"
 #include "../libtft/libtf.h"
 
+void ft_sendsignal(char *str, pid_t pid)
+{
+	int	i;
+
+	while(*str)
+	{
+		while(++i < 8)
+		{
+		i = -1;
+		if((*str >> i) % 2)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(1000);
+		}
+		str++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	pid_t		server_pid;
+	pid_t		pid;
 	struct sigaction	sa;
 
 	if (argc != 3)
-	{
-		ft_printf("Error, correct usage: %s <server_pid> <message>\n", argv[0]);
-		return (1);
-	}
-	server_pid = ft_atoi(argv[1]);
+		exit(ft_printf("Error, correct usage: %s <server_pid> <message>\n", argv[0]));
+	
+	pid = ft_atoi(argv[1]);
 	if (server_pid <= 0)
-	{
-		ft_printf("Error, <server_pid> must be higher than 0\n")
-		return (1);
-	}
-	sa.
+		exit(ft_printf("Error, <server_pid> must be higher than 0\n"));
+	sa.sa_handler = ft_sendsignal(argc[2], pid);
 }
